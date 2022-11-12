43d8f9d4f2f2c1f9b0e6b04ba97db9bb7571c83f
Belegos <wannefreakless@gmail.com>
2022-11-12 14:57:47 +0100

    Added GothicUI

--- Assets/Scripts/Player/PlayerLocomotion.cs
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -37,7 +37,7 @@ namespace KI_Project
             _moveDirection = _cameraObject.forward * _inputHandler.vertical;
             _moveDirection += _cameraObject.right * _inputHandler.horizontal;
             _moveDirection.Normalize();
-            //_moveDirection.y = 0;
+            _moveDirection.y = 0;
 
             float speed = movementSpeed;
             _moveDirection *= speed;
