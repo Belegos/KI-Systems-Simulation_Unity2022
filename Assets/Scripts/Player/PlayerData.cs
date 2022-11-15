36dfc8c4b7f1059030e407ece675329f683497ed
Belegos <wannefreakless@gmail.com>
2022-11-15 17:19:38 +0100

    Vor dem Schnee

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerData : MonoBehaviour
+public class PlayerData : MonoBehaviour, INotifyPropertyChanged
 {
-    [SerializeField][Range(0,100)] private int _currentHealth;
+    [SerializeField][Range(0, 100)] private int _currentHealth;
     [SerializeField][Range(0, 100)] private int _maxHealth;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshPro textComp;
+    [SerializeField] private GameObject SliderGameObject;
 
-
-    public int Health 
+    public event PropertyChangedEventHandler PropertyChanged;
+    public int Health
     {
         get { return _currentHealth; }
-        
-        set 
-        {  _currentHealth = value;
-            this.ValueChanged(this._value);
+
+        set
+        {
+            _currentHealth = value;
         }
     }
+
 
     public void TakeDamage(int damage)
     {
         Health -= damage;
+        PropertyChanged(this, new PropertyChangedEventArgs("Health"));
+    }
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        textComp = GetComponentInParent<TextMeshPro>();
     }
     private void Start()
     {
+        PropertyChanged(this, new PropertyChangedEventArgs("Health"));
         Health = _maxHealth;
     }
 }
