2641598b3ffdbd6bc6e01fa932c694198fd39deb
André <wannefreakless@gmail.com>
2022-12-30 18:11:21 +0100

    edited dungeon generation

--- Assets/Scripts/WorldGeneration/DungeonGenerator.cs
+++ Assets/Scripts/WorldGeneration/DungeonGenerator.cs
@@ -10,7 +10,8 @@ public class DungeonGenerator : MonoBehaviour
     private enum ERoomTypes
     {
         empty = -1,
-        normal
+        normal,
+        start
     }
     private const int MAP_WIDTH = 9;
     private const int MAP_HIGHT = 8;
@@ -52,7 +53,38 @@ public class DungeonGenerator : MonoBehaviour
     }
     private ERoomTypes[,] GenerateLevelToValidate(ERoomTypes[,] _baseLevel, int _numRoomsToGenerate)
     {
+        Queue<Vector2Int> positionsToExpand = new Queue<Vector2Int>(); //coordinates of rooms
+        Vector2Int midPos = new Vector2Int(MAP_WIDTH / 2, MAP_HIGHT / 2); //middle of level
+        _baseLevel[midPos.x, midPos.y] = ERoomTypes.start;
+        positionsToExpand.Enqueue(midPos);//addes midPos to Queue
+        int currRoomCount = 1;
 
+        while (positionsToExpand.Count > 0)
+        {
+            Vector2Int currPosToExpamd = positionsToExpand.Dequeue(); //dequeue's the first position of rooms and saves in local var
+            Vector2Int[] positionsToCheck = new Vector2Int[]
+            {
+                currPosToExpamd + Vector2Int.up,
+                currPosToExpamd + Vector2Int.right,
+                currPosToExpamd + Vector2Int.down,
+                currPosToExpamd + Vector2Int.left
+            };
+            for (int checkPosIdc = 0; checkPosIdc < positionsToCheck.Length; checkPosIdc++)
+            {
+                Vector2Int toCheck = positionsToCheck[checkPosIdc];
+
+                //check if in bounds
+                if (toCheck.x >= 0 && toCheck.x < MAP_WIDTH && toCheck.y >= 0 && toCheck.y < MAP_HIGHT) //checks if in bounds of array
+                {
+                    //Rule 1: if enough rooms => continue to next room
+
+                    //Rule 2:
+
+                    //Rule 3:
+
+                }
+            }
+        }
 
         return _baseLevel;
     }
