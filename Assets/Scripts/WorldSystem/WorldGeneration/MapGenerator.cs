b0bc853b16782cf2149b079d4eb1e620bd782bc4
André <wannefreakless@gmail.com>
2023-01-10 15:00:23 +0100

    added prng seed to noise generation,added slicer Persistance,added clamp

--- Assets/Scripts/WorldSystem/WorldGeneration/MapGenerator.cs
+++ Assets/Scripts/WorldSystem/WorldGeneration/MapGenerator.cs
@@ -8,14 +8,44 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    [Range(0, 1)] public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>(); //find Object in Scene
 
         display.DrawNoiseMap(noiseMap);//hand over the noiseMap
+    }
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        if(noiseScale < 0.00003f)
+        {
+            noiseScale = 0.00003f;
+        }
     }
 }
