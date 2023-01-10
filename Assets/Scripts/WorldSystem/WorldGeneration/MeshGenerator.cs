1ee287328df8bc574f30d5a5dab37cf014862638
André <wannefreakless@gmail.com>
2023-01-10 17:15:59 +0100

    LOD added, end of day push

--- Assets/Scripts/WorldSystem/WorldGeneration/MeshGenerator.cs
+++ Assets/Scripts/WorldSystem/WorldGeneration/MeshGenerator.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 
 public static class MeshGenerator
 {
-    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve)
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve,int levelOfDetail)
     {
         int width = heightMap.GetLength(0); //first dimension of the array to get the width
         int height = heightMap.GetLength(0);//second dimension of the array to get the height
         float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / 2f;
 
-        MeshData meshData = new MeshData(width, height);
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        //handles increament of the mesh simplification(LOD), if levelOfDetail is 0, then it is 1, else it is levelOfDetail * 2
+        int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
+
+        MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
         int vertexIndex = 0;
 
-        for (int y = 0; y < height; y++)
+        for (int y = 0; y < height; y+= meshSimplificationIncrement)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < width; x+= meshSimplificationIncrement)
             {
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x,heightCurve.Evaluate(heightMap[x, y])*heightMultiplier, topLeftZ - y); //x = x-coordinate, y = coordinate of the heightMap, z = y-coordinate
                 meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
                 
                 if (x < width - 1 && y < height - 1)//ignoring right and bottom verticies of the map
                 {
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
                     //i = vertexIndex 
                     //i + width + 1 = vertexIndex of the vertex to the right and down 
                     //i + width = vertexIndex of the vertex down
-                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
                 }
 
                 vertexIndex++; //Index to keep track, where we are in the 1D-Array
