f8ab808baa762ba09bdd39c8a4d5fb546ea65793
André <wannefreakless@gmail.com>
2023-01-23 21:52:58 +0100

    added UnityStandart1stPersonController

--- Assets/Scripts/WorldSystem/WorldGeneration/EndlessTerrain.cs
+++ Assets/Scripts/WorldSystem/WorldGeneration/EndlessTerrain.cs
@@ -79,6 +79,7 @@ public class EndlessTerrain : MonoBehaviour
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
+        MeshCollider meshCollider;
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
@@ -97,6 +98,7 @@ public class EndlessTerrain : MonoBehaviour
             meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshCollider = meshObject.AddComponent<MeshCollider>();
             meshRenderer.material = material;
 
             meshObject.transform.position = positionV3 * scale;
@@ -150,6 +152,7 @@ public class EndlessTerrain : MonoBehaviour
                         {
                             previousLODIndex = lodIndex;
                             meshFilter.mesh = lodMesh.mesh;
+                            meshCollider.sharedMesh = lodMesh.mesh;
                         }
                         else if (!lodMesh.hasRequestedMesh)
                         {
